ac3f835b5f01e315996c94702fc5f6cd245f9481
Benjamin <92789367+BenjaminOmar@users.noreply.github.com>
2024-04-05 08:52:02 +0200

    WIP

---
+++ Backend/Application/DependencyInjection.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Application.Services.Authentication;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Application
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddApplication(this IServiceCollection services)
+        {
+            services.AddScoped<IAuthenticationService, AuthenticationService>();
+            return services;
+        }
+    }
+}
--- Backend/Presentation/Program.cs
+++ Backend/Presentation/Program.cs
@@ -1,21 +1,27 @@
+using Application;
+using Application.Services.Authentication;
+
+
 var builder = WebApplication.CreateBuilder(args);
-
-builder.Services.AddControllers();
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-var app = builder.Build();
-
-if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    builder.Services.AddApplication();
+    builder.Services.AddInfrastructure();
+
+    builder.Services.AddControllers();
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen();
 }
 
-app.UseHttpsRedirection();
+var app = builder.Build();
+{
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+    }
 
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
+    app.UseHttpsRedirection();
+    //app.UseAuthorization();
+    app.MapControllers();
+    app.Run();
+}
