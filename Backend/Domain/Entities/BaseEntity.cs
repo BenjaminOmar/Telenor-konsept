d4d0463ea541bdf9408af067fc32468cd13e2977
Benjamin <92789367+BenjaminOmar@users.noreply.github.com>
2024-04-06 21:45:54 +0200

    Removed default values for modifiedon and createdon

--- Backend/Domain/Entities/BaseEntity.cs
+++ Backend/Domain/Entities/BaseEntity.cs
@@ -6,9 +6,9 @@ public abstract class BaseEntity
     
     public Guid CreatedBy { get; set; }
 
-    public DateTime CreatedOn { get; set; } = DateTime.UtcNow;
+    public DateTime CreatedOn { get; set; }
     
     public Guid ModifiedBy { get; set; }
     
-    public DateTime ModifiedOn { get; set; } = DateTime.UtcNow;
+    public DateTime ModifiedOn { get; set; }
 }
