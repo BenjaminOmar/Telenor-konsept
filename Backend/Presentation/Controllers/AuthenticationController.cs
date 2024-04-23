3ef1ac7fdaee922130e6c1bbc8b1539ba8cf95ab
Benjamin <92789367+BenjaminOmar@users.noreply.github.com>
2024-04-23 23:03:42 +0200

    Fikset typo

--- Backend/Presentation/Controllers/AuthenticationController.cs
+++ Backend/Presentation/Controllers/AuthenticationController.cs
@@ -34,7 +34,7 @@ namespace Presentation.Controllers
 
             if (result.IsSuccess)
             {
-                return Ok(new { Message = "Vellykket inlogging", Token = result.Value.AccessToken});
+                return Ok(new { Message = "Vellykket innlogging", Token = result.Value.AccessToken});
             }
 
             if (result.ErrorCode == 404)
