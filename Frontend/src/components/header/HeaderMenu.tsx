44cead569c038cca67f3cea27196f050d0f872e0
Benjamin <92789367+BenjaminOmar@users.noreply.github.com>
2024-05-14 21:16:39 +0200

    la til mitt eget profilbilde som header meny

--- Frontend/src/components/header/HeaderMenu.tsx
+++ Frontend/src/components/header/HeaderMenu.tsx
@@ -2,20 +2,13 @@ import {View, Text} from 'react-native';
 import React from 'react';
 import {useAuth} from '../../context/authContext';
 import {Avatar} from '@rneui/base';
+import Profilbilde from '../../../assets/images/profilbilde.png';
 
 const HeaderMenu = () => {
   const {onLogout} = useAuth();
   return (
     <>
-      <Avatar
-        size={50}
-        rounded
-        source={{uri: 'https://randomuser.me/api/portraits/men/36.jpg'}}
-        onPress={onLogout}
-      />
-      {/* <View>
-        <Text onPress={onLogout}>Logg ut</Text>
-      </View> */}
+      <Avatar size={50} rounded source={Profilbilde} onPress={onLogout} />
     </>
   );
 };
