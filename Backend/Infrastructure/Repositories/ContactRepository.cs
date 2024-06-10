4f1e89722b763d87391c51a607e7eeb5fd1f4004
Benjamin <92789367+BenjaminOmar@users.noreply.github.com>
2024-06-10 22:49:14 +0200

    lagt til noen dashboard kort

--- Backend/Infrastructure/Repositories/ContactRepository.cs
+++ Backend/Infrastructure/Repositories/ContactRepository.cs
@@ -10,18 +10,15 @@ using Domain.DTOs.Contact;
 
 namespace Infrastructure.Repositories;
 
-public class ContactRepository(DataContext context, IMapper _mapper) : BaseRepository<Contact>(context), IContactRepository
+public class ContactRepository(DataContext context, IMapper mapper) : BaseRepository<Contact>(context), IContactRepository
 {
-    private readonly DataContext _context = context;
-    private readonly IMapper _mapper = _mapper;
     private readonly IQueryable<Contact> _contactsQuery = context.Contacts.Where(c => !c.IsDeleted);
-    
     
     public async Task<List<ContactListResponseDto>> GetContactList(Guid customerId)
     {
         return await _contactsQuery
             .Where(c => c.CustomerId == customerId)
-            .ProjectTo<ContactListResponseDto>(_mapper.ConfigurationProvider)
+            .ProjectTo<ContactListResponseDto>(mapper.ConfigurationProvider)
             .ToListAsync();
     }
 }
